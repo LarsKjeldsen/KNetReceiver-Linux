bfa9a62fe073ee85d47a07acf85b30050b66a8e1
Lars Skovsende Kjeldsen <lars@kjeldsen.net>
2021-08-29 09:52:15 +0200

    Only update one mysql server with data.

--- KNetDatabase.cpp
+++ KNetDatabase.cpp
@@ -7,9 +7,7 @@ using namespace std;
 
 KNetDatabase::KNetDatabase()
 {
-	hostName_1 = "192.168.1.4";
-	hostName_2 = "192.168.1.20";
-	hostName_3 = "192.168.1.21";
+	hostName_1 = "192.168.1.21";
 	userId = "KNet";
 	password = "Minmore9876";
 	DB = "KNet";
@@ -20,16 +18,12 @@ KNetDatabase::KNetDatabase()
 KNetDatabase::~KNetDatabase()
 {
 	mysql_close(MySQLConnection_1);
-	mysql_close(MySQLConnection_2);
-	mysql_close(MySQLConnection_3);
 }
 
 
 void KNetDatabase::InsertReadings(const char* tabel_name, char* value)
 {
 	InsertReading(MySQLConRet_1, tabel_name, value);
-	InsertReading(MySQLConRet_2, tabel_name, value);
-	InsertReading(MySQLConRet_3, tabel_name, value);
 }
 
 void KNetDatabase::InsertReading(MYSQL* MySQLConRet, const char *tabel_name, char *value)
@@ -82,23 +76,19 @@ bool KNetDatabase::KNetConnect()
 	int Count = 0;
 	do {
 		MySQLConnection_1 = mysql_init(NULL);
-		MySQLConnection_2 = mysql_init(NULL);
-		MySQLConnection_3 = mysql_init(NULL);
 		MySQLConRet_1 = mysql_real_connect(MySQLConnection_1, hostName_1.c_str(), userId.c_str(), password.c_str(), DB.c_str(), 0, 0, 0);
-		MySQLConRet_2 = mysql_real_connect(MySQLConnection_2, hostName_2.c_str(), userId.c_str(), password.c_str(), DB.c_str(), 0, 0, 0);
-		MySQLConRet_3 = mysql_real_connect(MySQLConnection_3, hostName_3.c_str(), userId.c_str(), password.c_str(), DB.c_str(), 0, 0, 0);
 
-		if (MySQLConRet_1 == NULL || MySQLConRet_2 == NULL)
-			printf("ERROR in database connection\n 1- %s\n 2- %s\n 3-%s\n", mysql_error(MySQLConnection_1), mysql_error(MySQLConnection_2), mysql_error(MySQLConnection_3));
+		if (MySQLConRet_1 == NULL)
+			printf("ERROR in database connection\n 1- %s\n 2- %s\n 3-%s\n", mysql_error(MySQLConnection_1), mysql_error(MySQLConnection_2));
 		if (Count++ > 10)
 			break;
 
 		usleep(500000);
-	} while (MySQLConRet_1 == NULL || MySQLConRet_2 == NULL || MySQLConRet_3 == NULL);
+	} while (MySQLConRet_1 == NULL);
 
-	printf("MySQL Connection Info: %s - %s -%s\n", mysql_get_host_info(MySQLConnection_1), mysql_get_host_info(MySQLConnection_2), mysql_get_host_info(MySQLConnection_3));
+	printf("MySQL Connection Info: %s\n", mysql_get_host_info(MySQLConnection_1));
 
-	if (MySQLConRet_1 != NULL && MySQLConRet_2 != NULL && MySQLConRet_3 != NULL)
+	if (MySQLConRet_1 != NULL)
 		return true;
 	else
 		return false;
